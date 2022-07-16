129ccf44646e43c6417c6042c3df2f0625f1f16e
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-16 12:41:04 +0300

    configured express for static files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 5000;
@@ -6,5 +8,11 @@ const port = 5000;
 app.get('/', (req, res) => {
     res.send('Homepage');
 });
+app.engine('hbs', hbs.engine({ extname: 'hbs' }))
+app.set('view engine', 'hbs');
+app.set('views', path.resolve('src', 'views'))
+
+app.use('/public', express.static('public'));
+
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
