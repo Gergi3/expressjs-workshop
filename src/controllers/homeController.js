1b9e48670a0cb31ffd7b243894ae666ffe67d980
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-20 14:15:45 +0300

    search info now persists in input fields

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     const { search, from, to } = req.query;
     const cubes = await cubeServices.getAll(search, from, to);
 
-    res.render('home', { cubes });
+    res.render('home', { cubes, search, from, to });
 });
 
 exports.homeRouter = router;
