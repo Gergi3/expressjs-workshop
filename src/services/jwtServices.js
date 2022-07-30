9f4fa9ce6ee18b92d40020a21725e910c1fbfee0
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-30 19:27:54 +0300

    simplify promisifying of jwt callback functions

--- src/services/jwtServices.js
+++ src/services/jwtServices.js
@@ -1,5 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util')
+
 const secret = '7Jd3RSFB*6TyDS185g#3vm&NpfdPbWE#qv5Bp8qnp8^5%7%w0II@WwVTHVwFy1z908K5msfYVWs@30HOs3Ud^hy3K0b#L7$!uWq7pODl8d7rm&$t3D88d2wtGu2bMiwf5lcS6oF^uw*N24^!UW@*3Q';
+
+const promiseSign = promisify(jwt.sign);
+const promiseVerify = promisify(jwt.verify);
 
 exports.signToken = (data) => {
     const payload = data;
@@ -7,27 +12,9 @@ exports.signToken = (data) => {
         expiresIn: '2d',
     }
 
-    const promise = new Promise((resolve, reject) => {
-        jwt.sign(payload, secret, options, (err, token) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(token);
-        });
-    });
-    
-    return promise;
+    return promiseSign(payload, secret, options);
 } 
 
 exports.verifyToken = (token) =>{
-    const promise = new Promise((resolve, reject) => {
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(decoded);
-        });
-    });
-
-    return promise;
+    return promiseVerify(token, secret);
 } 
