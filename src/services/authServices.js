26540b5f6e1ea88c722c3a833221b3129840884c
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-31 21:55:25 +0300

    renamed utils to validators

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const { saltRounds } = require('../constants');
 const jwtServices = require('./jwtServices');
-const validators = require('../utils/authValidators');
+const validators = require('../validators/authValidators');
 
 exports.register = async ({username, password, repassword}) => {
     const users = await User.find({username});
